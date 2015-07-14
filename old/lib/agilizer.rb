babe2927c0629f59ea1ed73ae39aa66227ab0424
Romain Champourlier <pro@rchampourlier.com>
2015-07-14 15:38:39 +0200

    Removed old agile_jira implementation

--- old/lib/agilizer.rb
+++
@@ -1,9 +0,0 @@
-# Agilizer provides a datawarehouse service to store
-# data from bug and issue trackers in an homogeneous
-# format, providing several services on this data:
-#   - analytics,
-#   - monitoring and alerting,
-#   - etc.
-module Agilizer
-  VERSION = '0.0.1'
-end
