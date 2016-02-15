38904d2c63f781a1b07bcef4b4d877f404fcd077
Romain Champourlier <pro@rchampourlier.com>
2016-02-15 01:32:30 +0100

    Fix and optimisation for sprints_from_history enrichment

--- lib/agilizer/enrichments/sprints_from_history.rb
+++ lib/agilizer/enrichments/sprints_from_history.rb
@@ -1,3 +1,4 @@
+require 'hash_op'
 require 'rest-client'
 require 'agilizer/issue'
 require 'agilizer/interface/jira/transformations/3_add_sprint_information'
@@ -21,24 +22,24 @@ module Agilizer
     module SprintsFromHistory
       TRANSFORMATION = Interface::Jira::Transformations::AddSprintInformation
 
-      def run(issue_identifier)
+      def run(issue_identifier, sprints_info = nil)
         issue = Issue.where(identifier: issue_identifier).first
         return if issue.nil?
 
         processing_data = issue.attributes
         sprint_names = sprint_names_from_history(processing_data)
-        existing_sprint_names = processing_data['sprints'].map { |s| s['name'] }
-        missing_sprint_names = sprint_names - existing_sprint_names
+        existing_sprint_names = processing_data['sprints'].compact.map { |s| s['name'] }
+        missing_sprint_names = (sprint_names - existing_sprint_names).compact # TODO: cover with spec on JT-214
 
+        sprints_info ||= build_sprints_info
         enriched_sprints = missing_sprint_names.map do |sprint_name|
-          sprint = all_sprints.find { |s| s['name'] == sprint_name }
+          sprint = sprints_info.find { |s| s['name'] == sprint_name }
+          next if sprint.blank? # TODO: cover with spec on JT-2461
           sprint_information = TRANSFORMATION.sprint_information(processing_data, sprint)
           sprint.merge sprint_information
         end
 
-        processing_data.merge! 'sprints' => processing_data['sprints'] + enriched_sprints
-
-        issue.attributes = processing_data
+        issue.sprints = processing_data['sprints'] + enriched_sprints
         issue.save!
         issue
       end
@@ -55,15 +56,15 @@ module Agilizer
       # Returns the "sprint" Hash (name, started_at, ended_at, state)
       # for all sprints by fetching them from `Agilizer::Issue`
       # records.
-      def all_sprints
+      def build_sprints_info
         sprints = Agilizer::Issue.all.entries.map do |i|
           i['sprints']
         end
-        sprints.flatten.map do |h|
+        sprints.flatten.compact.map do |h|
           h.slice(*%w(name started_at ended_at state))
         end.uniq
       end
-      module_function :all_sprints
+      module_function :build_sprints_info
     end
   end
 end
