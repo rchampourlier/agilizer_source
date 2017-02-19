f09002bb9af18f473cc9211ca35ffa3dfdc9c6ac
JobTeaser Dev <dev@jobteaser.com>
2017-02-19 01:47:03 +0100

    [0.2.1] load pg_json_ops Sequel extension
    - Could not run script/process_cache otherwise

--- lib/agilizer/data.rb
+++ lib/agilizer/data.rb
@@ -4,6 +4,7 @@ require "sequel"
 module Agilizer
   module Data
     DATABASE_URL = ENV["DATABASE_URL"]
+    Sequel.extension :pg_json_ops
     DB = Sequel.connect(DATABASE_URL)
     DB.extension :pg_array, :pg_json
   end
