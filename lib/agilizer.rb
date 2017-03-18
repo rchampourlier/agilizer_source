8e4ebf20bb936c2317787bcdb96bb65457bad558
Romain Champourlier <pro@rchampourlier.com>
2017-03-18 17:00:19 +0100

    [0.3.0] minor codeclimate fixes

--- lib/agilizer.rb
+++ lib/agilizer.rb
@@ -5,7 +5,7 @@
 #   - monitoring and alerting,
 #   - etc.
 module Agilizer
-  VERSION = File.read(File.expand_path('../../VERSION', __FILE__)).strip
+  VERSION = File.read(File.expand_path("../../VERSION", __FILE__)).strip
 
   Error = Class.new(StandardError)
 end
